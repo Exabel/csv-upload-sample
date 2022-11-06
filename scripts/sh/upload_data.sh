4cc15947e1515461ef42d0f6aef06a0227b1b8c0
BoHuang2018 <tigerhuangbo@icloud.com>
2022-11-06 16:19:56 +0100

    Updated `scripts/sh/upload_data.sh` by adding create entity type, relationship type, corrected type error ('`' to `\`), and a comment session to show load_time_series_from_file works same as load_time_series_from_csv

--- scripts/sh/upload_data.sh
+++ scripts/sh/upload_data.sh
@@ -14,6 +14,12 @@ export EXABEL_NAMESPACE="my_namespace"
 # API key and namespace can be given explicitly through --api-key and --namespace parameters
 # They can also be given implicitly using the environment variables EXABEL_API_KEY and EXABEL_NAMESPACE
 
+# Create entity type
+python -m exabel_data_sdk.scripts.create_entity_type \
+    --api-key="$EXABEL_API_KEY" \
+		--name="entityTypes/$EXABEL_NAMESPACE.brand" --display-name="brand" \
+		--no-is-associative
+
 # Upload entities
 python -m exabel_data_sdk.scripts.load_entities_from_csv  \
     --api-key="$EXABEL_API_KEY" --namespace="$EXABEL_NAMESPACE" \
@@ -21,7 +27,12 @@ python -m exabel_data_sdk.scripts.load_entities_from_csv  \
     --name-column="brand" --display-name-column="display_name" \
     --description-column="description"
 
-# Upload relationships, or implicitly
+# Create relationship type, API key is given implicitly
+python -m exabel_data_sdk.scripts.create_relationship_type \
+		--name="relationshipTypes/$EXABEL_NAMESPACE.HAS_BRAND" \
+		--is-ownership
+
+# Upload relationships, API key is given implicitly
 python -m exabel_data_sdk.scripts.load_relationships_from_csv  \
     --filename="./resources/data/relationships/HAS_BRAND.csv" \
     --entity-from-column="factset_identifier" --entity-to-column="brand" \
@@ -39,6 +50,24 @@ python -m exabel_data_sdk.scripts.load_time_series_from_csv  \
 #    --pit-offset=1 --create-missing-signals
 
 # Upload time series on a daily/weekly/monthly schedule, setting known time to the current time (upload time)
-python -m exabel_data_sdk.scripts.load_time_series_from_csv  `
-    --filename="./resources/data/time_series/brand_time_series_one_day.csv" `
+python -m exabel_data_sdk.scripts.load_time_series_from_csv \
+    --filename="./resources/data/time_series/brand_time_series_one_day.csv" \
     --pit-current-time
+
+# The above time series uploading can be done by module
+# exabel_data_sdk.scripts.load_time_series_from_file with the same parameters
+#
+# Upload time series with known time
+# python -m exabel_data_sdk.scripts.load_time_series_from_file  \
+#     --filename="./resources/data/time_series/brand_time_series.csv" \
+#     --create-missing-signals
+#
+# Or upload using PiT offset
+# python -m exabel_data_sdk.scripts.load_time_series_from_file  \
+#     --filename="./resources/data/time_series/brand_time_series_without_known_time.csv" \
+#     --pit-offset=1 --create-missing-signals
+#
+# Upload time series on a daily/weekly/monthly schedule, setting known time to the current time (upload time)
+# python -m exabel_data_sdk.scripts.load_time_series_from_file \
+#     --filename="./resources/data/time_series/brand_time_series_one_day.csv" \
+#     --pit-current-time
